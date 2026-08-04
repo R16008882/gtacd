fbcb141ba6a640b1939b4a52520f575f559fa699
rachel-koehler <rachel.koehler@laplateforme.io>
2026-08-04 16:52:18 +0200

    indentation correction

--- app/check_app.sh
+++ app/check_app.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
+
 echo "[INFO] Début de la vérification de l'application..."
+
 if [ -f "./app/index.html" ]; then
-echo "[OK] Le fichier app/index.html est présent."
-echo "[INFO] Vérification terminée avec succès."
-exit 0
+    echo "[OK] Le fichier app/index.html est présent."
+    echo "[INFO] Vérification terminée avec succès."
+    exit 0
 else
-echo "[ERREUR] Le fichier app/index.html est introuvable."
-echo "[INFO] Vérification terminée avec échec."
-exit 1
+    echo "[ERREUR] Le fichier app/index.html est introuvable."
+    echo "[INFO] Vérification terminée avec échec."
+    exit 1
 fi
